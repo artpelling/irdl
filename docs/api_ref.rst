a6ccb4912f90eaaa8d547a52852e1a40af089b38
Art Pelling <a.pelling@tu-berlin.de>
2026-01-26 17:08:41 +0100

    wip

--- docs/api_ref.rst
+++ docs/api_ref.rst
@@ -1,2 +1,21 @@
 API Reference
 =============
+
+.. currentmodule:: irdl
+
+Internals
+---------
+.. autosummary::
+    :toctree: _auto_summary
+
+    cli
+    downloader
+    repositories
+
+Datasets
+--------
+.. autosummary::
+    :toctree: _auto_summary
+
+    fabian
+    miracle
--- docs/conf.py
+++ docs/conf.py
@@ -5,4 +5,19 @@ author = irdl.__author__
 copyright = f"{irdl.__date__.split(' ')[-1]}, {irdl.__author__}"
 version = irdl.__version__
 
+extensions = [
+    "numpydoc",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+]
+
 html_theme = "pydata_sphinx_theme"
+
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": True,
+    "private-members": True,
+    "show-inheritance": True,
+}
+
+autosummary_generate = True
