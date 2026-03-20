4b37e6df4f61a451205f7095e80958ec3b76d01f
Art Pelling <account@artpelling.de>
2026-03-20 17:45:55 +0800

    linting

--- src/irdl/__init__.py
+++ src/irdl/__init__.py
@@ -1,8 +1,8 @@
 """Impulse Response Downloader (irdl): Download, unpack and process impulse response datasets."""
 
 from .downloader import CACHE_DIR as CACHE_DIR
-from .sofa import get_fabian as get_fabian
 from .ista import get_miracle as get_miracle
 from .ista import get_sriracha as get_sriracha
+from .sofa import get_fabian as get_fabian
 
 __all__ = ["get_fabian", "get_miracle", "get_sriracha"]
