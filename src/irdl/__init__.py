43679d1df03525cde2a4ebb824063797498963df
Art Pelling <account@artpelling.de>
2026-04-10 14:21:50 +0200

    version

--- src/irdl/__init__.py
+++ src/irdl/__init__.py
@@ -1,8 +1,6 @@
 """Impulse Response Downloader (irdl): Download, unpack and process impulse response datasets."""
 
 __author__ = "Art Pelling"
-__date__ = "23 January 2026"
-__version__ = "0.1.0"
 
 from .downloader import CACHE_DIR as CACHE_DIR
 from .fabian import get_fabian as get_fabian
