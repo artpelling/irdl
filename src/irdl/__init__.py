a5c76ba14ace13def60fe55f551638edaa317345
Art Pelling <account@artpelling.de>
2026-09-24 11:09:54 +0200

    formatting

--- src/irdl/__init__.py
+++ src/irdl/__init__.py
@@ -7,12 +7,12 @@ from .akt import BrasRs8Dataset as BrasRs8Dataset
 from .akt import FabianDataset as FabianDataset
 from .akt import HutubsDataset as HutubsDataset
 from .base import _get_dataset_classes as _get_dataset_classes
-from .sonicom import DechorateDataset as DechorateDataset
 from .esat import MyriadDataset as MyriadDataset
-from .ista import MiracleDataset as MiracleDataset
 from .iks import MirdDataset as MirdDataset
+from .ista import MiracleDataset as MiracleDataset
 from .ista import SrirachaDataset as SrirachaDataset
 from .sonicom import CipicDataset as CipicDataset
+from .sonicom import DechorateDataset as DechorateDataset
 from .sonicom import SadieDataset as SadieDataset
 
 __all__ = [dataset_class.__name__ for dataset_class in _get_dataset_classes(_sys.modules[__name__])]
